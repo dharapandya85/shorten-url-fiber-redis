b09092fa333dabc7e623b80dbb600b30dca51adc
Dhara <dharapandya2020@gmail.com>
2025-04-07 13:14:35 +0000

    creating resolve file

---
+++ api/routes/shorten.go
@@ -0,0 +1,47 @@
+package routes
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type request struct {
+	URL         string        `json:"url"`
+	CustomShort string        `json:"short"`
+	Expiry      time.Duration `json:"expiry"`
+}
+
+type response struct {
+	URL            string        `json:"url"`
+	CustomShort    string        `json:"short"`
+	Expiry         time.Duration `json:"expiry"`
+	XRateRemaining int           `json:"rate_limit"`
+	XRateLimitRest time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+	// Parse request body
+	body := new(request)
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse JSON",
+		})
+	}
+
+	//implement rate limiting
+	// Check if input is an actual URL
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "invalid URL",
+		})
+	}
+	//check for domain error
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+			"error": "invalid domain",
+		})
+	}
+	//enforce https, SSL
+	body.URL = helpers.EnforceHTTP(body.URL)
+}
